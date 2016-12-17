eaf835d51d0587277fce008bd4295b7ea5849f0b
Richarbe <benr239@gmail.com>
2016-12-16 17:12:22 -0800

    Added Launch Attempt
    fixed some stuff too

--- model/launch_attempt_dal.js
+++ model/launch_attempt_dal.js
@@ -5,16 +5,16 @@ var db  = require('./db_connection.js');
 var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM vehicle_full;';
+    var query = 'SELECT * FROM launch_attempt_full;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.getById = function(vehicle_id, callback) {
-    var query = 'SELECT * FROM vehicle_full WHERE vehicle_id = ?';
-    var queryData = [vehicle_id];
+exports.getById = function(launch_attempt_id, callback) {
+    var query = 'SELECT * FROM launch_attempt_full WHERE launch_attempt_id = ?';
+    var queryData = [launch_attempt_id];
     console.log(query);
 
     connection.query(query, queryData, function(err, result) {
@@ -25,9 +25,9 @@ exports.getById = function(vehicle_id, callback) {
 
 exports.insert = function(params, callback) {
 
-    var query = 'INSERT INTO vehicle (veh_name, vehicle_model_id, organization_id) VALUES (?,?,?)';
+    var query = 'INSERT INTO launch_attempt (launch_time, success_status, launch_pad_id, vehicle_id) VALUES (?,?,?,?)';
 
-    var queryData = [params.veh_name, params.vehicle_model_id, params.organization_id];
+    var queryData = [params.launch_time, params.success_status, params.launch_pad_id, params.vehicle_id];
 
     connection.query(query, queryData, function(err, result){
         callback(err, result);
@@ -36,17 +36,17 @@ exports.insert = function(params, callback) {
 };
 
 exports.update = function(params, callback) {
-    var query = 'UPDATE vehicle SET veh_name = ?, vehicle_model_id = ?, organization_id = ? WHERE vehicle_id = ?';
-    var queryData = [params.veh_name, params.vehicle_model_id, params.organization_id, params.vehicle_id];
+    var query = 'UPDATE launch_attempt SET launch_time = ?, success_status = ?, launch_pad_id = ?, vehicle_id = ? WHERE launch_attempt_id = ?';
+    var queryData = [params.launch_time, params.success_status, params.launch_pad_id, params.vehicle_id, params.launch_attempt_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.delete = function(vehicle_id, callback) {
-    var query = 'DELETE FROM vehicle WHERE vehicle_id = ?';
-    var queryData = [vehicle_id];
+exports.delete = function(launch_attempt_id, callback) {
+    var query = 'DELETE FROM launch_attempt WHERE launch_attempt_id = ?';
+    var queryData = [launch_attempt_id];
 
     connection.query(query, queryData, function (err, result) {
         callback(err, result);
--- routes/launch_attempt_routes.js
+++ routes/launch_attempt_routes.js
@@ -1,5 +1,135 @@
 var express = require('express');
 var router = express.Router();
+var launch_attempt_dal = require('../model/launch_attempt_dal');
+var vehicle_dal = require('../model/vehicle_dal');
+var launch_pad_dal = require('../model/launch_pad_dal');
+
+// View All accounts
+router.get('/all', function(req, res) {
+    launch_attempt_dal.getAll(function(err, result){
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.render('launch_attempt/launch_attemptViewAll', { 'result':result });
+        }
+    });
+
+});
+
+// View the account for the given id
+router.get('/', function(req, res){
+    if(req.query.launch_attempt_id == null) {
+        res.send('launch_attempt_id is null');
+    }
+    else {
+        launch_attempt_dal.getById(req.query.launch_attempt_id, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('launch_attempt/launch_attemptViewById', {'result': result});
+            }
+        });
+    }
+});
+
+// Return the add a new company form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    vehicle_dal.getAll(function(err,vehicle) {
+        launch_pad_dal.getAll(function(err,launch_pad) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('launch_attempt/launch_attemptAdd', {'vehicle':vehicle, 'launch_pad':launch_pad});
+            }
+        });
+    });
+});
+
+// insert a company record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.launch_time == null) {
+        res.send('A launch time must be provided.');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        launch_attempt_dal.insert(req.query, function(err,result) {
+            if (err) {
+                console.log(err)
+                res.send(err);
+            }
+            else {
+                //poor practice for redirecting the user to a different page, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/launch_attempt/all');
+            }
+        });
+    }
+});
+
+router.get('/edit', function(req, res){
+    if(req.query.launch_attempt_id == null) {
+        res.send('An launch attempt id is required');
+    }
+    else {
+        launch_attempt_dal.getById(req.query.launch_attempt_id, function(err,launch_attempt) {
+            vehicle_dal.getAll(function(err,vehicle) {
+                launch_pad_dal.getAll(function(err,launch_pad) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    else {
+                        res.render('launch_attempt/launch_attemptUpdate', {
+                            'launch_attempt': launch_attempt,
+                            'vehicle': vehicle,
+                            'launch_pad': launch_pad
+                        });
+                    }
+                });
+            });
+        });
+    }
+
+});
+
+router.get('/update', function(req, res) {
+    if(req.query.launch_attempt_id == null) {
+        res.send('An launch attempt id is required');
+    }
+    else {
+        launch_attempt_dal.update(req.query, function(err, result){
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.redirect(302, '/launch_attempt/all');
+            }
+        });
+    }
+});
+
+// Delete an launch_attempt for the given launch_attempt_id
+router.get('/delete', function(req, res){
+    if(req.query.launch_attempt_id == null) {
+        res.send('launch_attempt_id is null');
+    }
+    else {
+        launch_attempt_dal.delete(req.query.launch_attempt_id, function(err, result){
+
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/launch_attempt/all');
+            }
+
+        });
+    }
+});
 
 
 module.exports = router;
